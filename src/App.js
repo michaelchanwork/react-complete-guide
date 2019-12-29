d44254f8c02ea076f1d21c1ff4bd7a0568660bbe
Michael Chan <michaelchanworkmc@gmail.com>
2019-12-29 13:28:58 -0800

    committing the rest of the files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +19,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>This is the start of Michael's adventure into the React course!</h1>
+      <Person />
     </div>
   );
 }
