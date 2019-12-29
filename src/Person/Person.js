585f6f6130619f52a4bfab202aef6d6088408b7c
Michael Chan <michaelchanworkmc@gmail.com>
2019-12-29 13:25:32 -0800

    Part 32 - Start of Person Component

---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+//Person component
+import React from 'react'
+
+const person = () => {
+    return(
+        <div className="Person">
+            <p>I'm a person!</p>
+        </div>
+    );
+};
+
+export default person;
